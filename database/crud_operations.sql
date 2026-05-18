7cad36e7edcb89d1d8eda1d7abd5667a9419c0af
kguido798 <g.kayigamba@alustudent.com>
2026-05-18 15:36:44 +0200

    proper queries

---
+++ database/crud_operations.sql
@@ -0,0 +1,5 @@
+INSERT INTO users(full_name, phone_number, email)
+VALUES ('Chrismel Amani', '0782833134', 'c.amani@alustudent.com');
+SELECT * FROM users;
+update transactions set amount = 6000 where transaction_is = 3;
+DELETE FROM system_logs where log_id = 5;
