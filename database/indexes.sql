de6b70f539bb3c538be0a615b6bf06cee66858b2
kguido798 <g.kayigamba@alustudent.com>
2026-05-18 13:54:07 +0200

    Their index

---
+++ database/indexes.sql
@@ -0,0 +1,3 @@
+CREATE INDEX idx_phone_number ON users(phone_number);
+CREATE INDEX idx_transaction_date ON transactions(transaction_date);
+CREATE INDEX idx_category_id ON transactions(category_id);
