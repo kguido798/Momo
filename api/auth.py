f42d1390115f434f96cf9f8efc4a5a0ae648a798
NYIRIHIRWE Yves <n.yves@alustudent.com>
2026-05-28 15:19:17 +0200

    Implement basic authentication in auth.py

---
+++ api/auth.py
@@ -0,0 +1,29 @@
+
+import base64
+
+USERNAME = "admin"
+PASSWORD = "devnote"
+
+
+def authenticate(headers):
+    auth_header = headers.get("Authorization")
+
+    if not auth_header:
+        return False
+
+    try:
+        auth_type, encoded_credentials = auth_header.split(" ")
+
+        if auth_type != "Basic":
+            return False
+
+        decoded_credentials = base64.b64decode(
+            encoded_credentials
+        ).decode("utf-8")
+
+        username, password = decoded_credentials.split(":")
+
+        return username == USERNAME and password == PASSWORD
+
+    except Exception:
+        return False
