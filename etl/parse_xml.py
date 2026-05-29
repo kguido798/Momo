1052b39eaecd68c899d607e2178ad7ad1e2bda88
kguido798 <g.kayigamba@alustudent.com>
2026-05-29 02:21:06 +0200

    my parser

--- etl/parse_xml.py
+++ etl/parse_xml.py
@@ -1,1 +1,28 @@
-# xml parsing placeholder
+import xml.etree.ElementTree as ET
+import json
+
+XML_FILE = "momo.xml"
+JSON_FILE = "../api/data.json"
+
+tree = ET.parse(XML_FILE)
+root = tree.getroot()
+
+transactions = []
+
+for transaction in root.findall("transaction"):
+
+    record = {
+        "id": int(transaction.find("id").text),
+        "type": transaction.find("type").text,
+        "amount": int(transaction.find("amount").text),
+        "sender": transaction.find("sender").text,
+        "receiver": transaction.find("receiver").text,
+        "timestamp": transaction.find("timestamp").text
+    }
+
+    transactions.append(record)
+
+with open(JSON_FILE, "w") as json_file:
+    json.dump(transactions, json_file, indent=4)
+
+print("XML successfully converted to JSON")
